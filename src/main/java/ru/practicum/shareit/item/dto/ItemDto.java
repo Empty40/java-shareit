c2c0bf5e8dc3bb676ca713f70ac8046ff178aef0
Empty40 <Sorok041@gmail.com>
2023-08-02 18:31:15 +0300

    ТЗ 13 - 1 попытка

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,29 @@
 package ru.practicum.shareit.item.dto;
 
-/**
- * TODO Sprint add-controllers.
- */
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+import ru.practicum.shareit.user.dto.UserDto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
 public class ItemDto {
+    @Positive(message = "id не может быть меньше нуля!")
+    private Long id;
+    @NotBlank(message = "name не может быть пустым!")
+    private String name;
+    @NotBlank(message = "description не может быть пустым!")
+    private String description;
+    @NotNull(message = "available не может быть пустым!")
+    private Boolean available;
+    private UserDto owner;
+    private ItemRequestDto request;
 }
