42e6d8e677b445dc1a814bf589ec6cd0d31e4eee
Empty40 <Sorok041@gmail.com>
2023-08-14 00:10:19 +0300

    ТЗ 13 - 2 попытка

--- src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.item.mapper;
 
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.request.mapper.RequestMapper;
 
 public class ItemMapper {
 
